ac0fa4709b257099863997a2e1c693861b596685
DeVinci <58373204+sudoDeVinci@users.noreply.github.com>
2021-06-19 10:17:59 -0700

    Simple Ascii Art generator

--- aswim.py
+++ aswim.py
@@ -1,12 +1,19 @@
 import cv2
+from datetime import datetime
+import os
 
 
-ASCII_CHARS=["@","#","&","¤","W","M","N","£","$","%","O","X","L","(","/","_","+","!",";",":","*","-",",","."," "]
+#-----------------------------------------------#
+                #Set Ascii Array By Intensity#
+#-----------------------------------------------#
+
+
+ASCII_CHARS=["@","&","#","¤","W","M","N","£","$","%","O","X","L","/","=","+","_","!",";",":","*","~","-",",","."," "]
 ASCII_CHARS.reverse()
 
 
 #-----------------------------------------------#
-                    #Initialize Video#
+                #Initialize Video#
 #-----------------------------------------------#
 
 
@@ -19,20 +26,21 @@ frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 
 #-----------------------------------------------#
-                    #TrackBar#
+                #TrackBar#
 #-----------------------------------------------#
 
 
 def nothing():pass
 
 cv2.namedWindow("Playback")
-cv2.createTrackbar("Height","Playback",54,100,nothing)
-cv2.createTrackbar("Width","Playback",208,300,nothing)
-cv2.createTrackbar("FrameTime","Playback",12,50,nothing)
+cv2.createTrackbar("Height","Playback",56,100,nothing)
+cv2.createTrackbar("Width","Playback",202,300,nothing)
+cv2.createTrackbar("FrameTimeNoProcessing","Playback",13,50,nothing)
+#cv2.createTrackbar("FrameTimeProcessing","Playback",0,10000,nothing)
 
 
 #-----------------------------------------------#
-                    #Main While Loop#
+                #Main While Loop#
 #-----------------------------------------------#
 
 
@@ -40,15 +48,18 @@ while playing:
 
     height = cv2.getTrackbarPos("Height","Playback")
     width = cv2.getTrackbarPos("Width","Playback")
-    frametime = cv2.getTrackbarPos("FrameTime","Playback")
+    frametime = cv2.getTrackbarPos("FrameTimeNoProcessing","Playback")
     if frametime == 0:
         frametime = 1
 
     playing,frame = cap.read()
+
+    #start = float(datetime.now().strftime('%f'))
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
+
     #-----------------------------------------------#
-                        #Scaling#
+                    #Scaling#
     #-----------------------------------------------#
 
 
@@ -58,24 +69,36 @@ while playing:
     
     
     #-----------------------------------------------#
-                        #Image Iteration#
+                    #Image Iteration#
     #-----------------------------------------------#
     
-    char_multi_row = "\n"
+
+    
+    char_multi_row = ""
     for row in frame:
         
         char_row=[]
 
         for pixel in row:
-            char_value = int(pixel/11)
+            char_value = int(pixel/10.8)
             char_row.append(ASCII_CHARS[char_value])
         
         char_multi_row += "".join(char_row)
         char_multi_row += "\n"
 
-    print(char_multi_row)
+    #delta = float(datetime.now().strftime('%f')) - start
+    #delta = int(delta)
+    #cv2.setTrackbarPos("FrameTimeProcessing","Playback",delta)
+
+    
+    print(f"{char_multi_row}")
 
     cv2.imshow("Video Out",frame)
     if cv2.waitKey(frametime) & 0xFF == ord('q'):
         break
+
+
+
+
+
 
